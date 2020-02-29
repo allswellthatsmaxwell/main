8540aa432c6ad932ee7fcaf9babfc4c25d7c4e2d
EC2 Default User <ec2-user@ip-172-31-44-216.us-east-2.compute.internal>
2020-02-29 18:29:07 +0000

    install scripts

---
+++ docker/install_and_start_docker.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+## Need to run as sudo.
+yum install docker
+usermod -a -G docker ec2-user ## if running Amazon Linux
+service docker start
---
+++ docker/run_docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+port=$1
+docker pull jupyter/datascience-notebook:latest
+docker run -it --rm \
+       -v /home/ec2-user/main:/home/jovyan/main \
+       -p $port:$port \
+       --name "maxwell" \
+       -e GRANT_SUDO=yes \
+       -e NOTEBOOK_PORT=$port \
+       -e NB_UID=1000 \
+       -e USERID=1000 \
+       -e JUPYTER_ENABLE_LAB=yes \
+       jupyter/datascience-notebook:latest jupyter lab --NotebookApp.token=''
+
+# jupyter/datascience-notebook:latest start.sh jupyter lab --NotebookApp.token='' --NotebookApp.port=$port
+       
+       
+       
+       
