52a355c89a1d5f8f0f774771db237abe1ad58f4a
Maxwell Peterson <allswellthatsmaxwell9@gmail.com>
2020-03-09 05:14:35 +0000

    set memory limit; messed with image libs

--- docker/julia/run_docker.sh
+++ docker/julia/run_docker.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 port=$1
 
-image="julia:latest"
-docker pull $image
+image="julia_with_image_libs:latest"
+# image="julia:latest"
+## docker pull $image
 docker run -it --entrypoint /bin/bash \
        -v /home/ec2-user/main:/home/jovyan/main \
        -p $port:$port \
+       --m=1.5G \
        --name "maxwell" \
        -e GRANT_SUDO=yes \
        -e USERID=1000 \
-       julia:latest
+       $image
