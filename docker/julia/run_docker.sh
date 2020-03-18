ca3114f28d00371433c82439e48e35ae469e7b82
Maxwell Peterson <allswellthatsmaxwell9@gmail.com>
2020-03-18 04:39:12 +0000

    adding julia startup file

--- docker/julia/run_docker.sh
+++ docker/julia/run_docker.sh
@@ -4,9 +4,12 @@ port=$1
 image="julia_with_image_libs:latest"
 # image="julia:latest"
 ## docker pull $image
+
+ ## will need to make config dir for startup.jl.
 docker run -it --rm --entrypoint /bin/bash \
        -v ~/main:/root/main \
        -v ~/.aws:/root/.aws \
+       -v ~/main/setup/startup.jl:/root/.julia/config/startup.jl \
        -p $port:$port \
        -m=1.5G \
        --name "maxwell" \
---
+++ setup/startup.jl
@@ -0,0 +1,11 @@
+using Pkg
+Pkg.instantiate()
+
+atreplinit() do repl
+    try
+        @eval using Revise
+        @async Revise.wait_steal_repl_backend()
+    catch e
+        @warn(e.msg)
+    end
+end
