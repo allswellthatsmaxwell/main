8166a3e81f0b66994f77cfe5cbceaee0ac0ab646
Maxwell Peterson <allswellthatsmaxwell9@gmail.com>
2020-03-28 04:29:38 +0000

    added data transforms and config and data objects

--- projects/Songs/src/Songs.jl
+++ projects/Songs/src/Songs.jl
@@ -13,8 +13,41 @@ LYRICS_ZIPNAME = "data/380000-lyrics-from-metrolyrics.zip"
 LOCAL_DATA_DIR = "data"
 DEFAULT_DATA_FPATH = "$(LOCAL_DATA_DIR)/$(basename(LYRICS_ZIPNAME))"
 VERBOSE = true
+
+struct CharMap
+    """ Maps characters in song lyrics to integers. """
+    d::Dict{Char, Int}
+end
+
+struct ArtistMap
+    """ Maps artist names to integers. """
+    d::Dict{String, Int}
+end
+
+struct ModelConfig
+    in::Int
+    out::Int
+    pad::Int
+end
+
+struct ModelData
+    X::Array{Int, 2}
+    Y::Flux.OneHotMatrix{Array{Flux.OneHotVector, 1}}
+    cfg::ModelConfig
+end
+
+function ModelData(df::DataFrame)
+    cfg = ModelConfig(df)
+    Y = Flux.onehotbatch(df[!, :artist], unique(df[!, :artist]))
+    cmap = CharMap(df[!, :lyrics])
+    encoded_lyrics = encode_and_pad(cmap, df[!, :lyrics], cfg)
+    X = hcat(encoded_lyrics...)
+    return ModelData(X, Y, cfg)
+end
+
 function download(; s3bucket = BUCKET_NAME, s3path = LYRICS_ZIPNAME,
                   outpath = DEFAULT_DATA_FPATH)
+    """ Downloads a file from AWS S3 to local disk. """
     ### doesn't work :/
     if !isfile(outpath)
         if VERBOSE
@@ -28,6 +61,24 @@ function download(; s3bucket = BUCKET_NAME, s3path = LYRICS_ZIPNAME,
         s3_get_file(aws, s3bucket, s3path, outpath)
     end
 end
+
+
+function read_and_process_data(;max_rows::Int = 10000,
+                               train_prop::Float64 = 0.80)::DataFrame
+    """ 
+    Reads the MetroLyrics file, removes rows where the lyrics are missing,
+    does a little standardization on lyrics, and marks rows for train and test
+    split.
+    """
+    data = first(CSV.read("data/lyrics.csv"),
+                 max_rows)
+    data = filter(row -> !ismissing(row[:lyrics]), data)
+    data[!, :lyrics] = standardize(data[!, :lyrics])
+    data = mark_train_test_by_artist(data, train_prop)
+    data[!, :rowid] = 1:nrow(data)
+    return data
+end
+
 
 function count_songs_per_artist(data::DataFrame)::DataFrame
     return by(data, :artist, :song => length) |>
@@ -78,10 +129,6 @@ standardize(lyrics::Array{Union{String, Missing}, 1}) = map(lyrics) do s
     end
 end
 
-struct CharMap
-    d::Dict{Char, Int}
-end
-
 function CharMap(lyrics::Array{String, 1})
     d = Dict()
     i = 1
@@ -96,9 +143,26 @@ function CharMap(lyrics::Array{String, 1})
     return CharMap(d)
 end
 
-struct ArtistMap
-    d::Dict{String, Int}
+encode(charmap::CharMap, songtext::String)::Array{Int, 1} = [
+    charmap.d[c] for c in songtext]
+
+encode(charmap::CharMap, songtexts::Array{String, 1})::Array{Array{Int, 1}, 1} =
+    [encode(charmap, songtext) for songtext in songtexts]
+
+function encode_and_pad(charmap::CharMap, songtext::String,
+                        cfg::ModelConfig)::Array{Int, 1}
+    unpadded::Array{Int, 1} = encode(charmap, songtext)
+    remaining::Int = cfg.in - length(unpadded)
+    padding = repeat([cfg.pad], remaining)
+    return vcat(unpadded, padding)   
 end
+
+function encode_and_pad(charmap::CharMap, songtexts::Array{String, 1},
+                        cfg::ModelConfig)::Array{Array{Int, 1}, 1}
+    return [encode_and_pad(charmap, songtext, cfg) for songtext in songtexts]
+end
+
+
 
 function ArtistMap(artists::Array{String, 1})
     d = Dict()
@@ -115,39 +179,34 @@ end
 encode(amap::ArtistMap, artists::Array{String, 1})::Array{Int, 1} = [
     amap.d[artist] for artist in artists]
 
-encode(charmap::CharMap, songtext::String)::Array{Int, 1} = [
-    charmap.d[c] for c in songtext]
+ModelConfig(in::Int, out::Int) = ModelConfig(in, out, 0)
 
-encode(charmap::CharMap, songtexts::Array{String, 1})::Array{Array{Int, 1}, 1} =
-    [encode(charmap, songtext) for songtext in songtexts]
-
-function read_and_process_data(;max_rows::Int = 10000,
-                               train_prop::Float64 = 0.80)::DataFrame    
-    data = first(CSV.read("data/lyrics.csv"),
-                 max_rows)
-    data = filter(row -> !ismissing(row[:lyrics]), data)
-    data[!, :lyrics] = standardize(data[!, :lyrics])
-    data = mark_train_test_by_artist(data, train_prop)
-    data[!, :rowid] = 1:nrow(data)
-    return data
+function ModelConfig(df::DataFrame)
+    max_obs_len = maximum(map(length, df[!, :lyrics]))
+    n_artists = length(unique(df[!, :artist]))
+    return ModelConfig(max_obs_len, n_artists)
 end
 
-function make_model(encoded_lyrics::Array{Array{Int,1}, 1},
-                    encoded_artists::Array{Int, 1})
-    return nothing
+function construct_model(in::Int, out::Int)
+    return Chain(Dense(in,  out, tanh),
+                 Dense(out, out, relu),
+                 Dense(out, out, tanh),
+                 Dense(out, out, relu),
+                 Dense(out, out, tanh),
+                 Dense(out, out, σ))
 end
 
-function make_model(df::DataFrame)
-    amap = ArtistMap(df[!, :artist])
-    cmap = CharMap(df[!, :lyrics])
-    encoded_lyrics = encode(cmap, df[!, :lyrics])
-    encoded_artists = encode(amap, df[!, :artist])
-    return make_model(encoded_lyrics, encoded_artists)
+construct_model(cfg::ModelConfig) = construct_model(cfg.in, cfg.out)
+
+function getvars()
+    df = read_and_process_data()
+    mdata = ModelData(df)
+    model = construct_model(mdata.cfg)
+    return df, mdata, model
 end
 
-    
-
-# cm = CharMap(data[!, :lyrics])
+#using Revise, Songs
+#df, mdata, model = Songs.getvars()
 
 ## struct EmbeddingLayer
 ##     W
