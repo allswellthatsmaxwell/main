3d445741f36afa3371b0c1355da7b04213592f47
Maxwell Peterson <allswellthatsmaxwell9@gmail.com>
2020-04-01 03:43:52 +0000

    fixed Chain spec; matched X dims

--- projects/Songs/src/Songs.jl
+++ projects/Songs/src/Songs.jl
@@ -39,14 +39,22 @@ struct ModelData
     cfg::ModelConfig
 end
 
-function ModelData(df::DataFrame)
-    cfg = ModelConfig(df)
+function ModelData(df::DataFrame, cfg::ModelConfig)
+    """
+    Constructs a ModelData where X is of shape ... TODO    
+    """
     Y = Flux.onehotbatch(df[!, :artist], unique(df[!, :artist]))
     cmap = CharMap(df[!, :lyrics])
     encoded_lyrics = encode_and_pad(cmap, df[!, :lyrics], cfg)
     X = hcat(encoded_lyrics...)
     return ModelData(X, Y, cfg)
 end
+
+function ModelData(df::DataFrame)
+    cfg = ModelConfig(df)
+    return ModelData(df, cfg)
+end
+
 
 function download(; s3bucket = BUCKET_NAME, s3path = LYRICS_ZIPNAME,
                   outpath = DEFAULT_DATA_FPATH)
@@ -66,7 +74,7 @@ function download(; s3bucket = BUCKET_NAME, s3path = LYRICS_ZIPNAME,
 end
 
 
-function read_and_process_data(;max_rows::Int = 10000,
+function read_and_process_data(;max_rows::Int = 1000,
                                train_prop::Float64 = 0.80)::DataFrame
     """ 
     Reads the MetroLyrics file, removes rows where the lyrics are missing,
@@ -156,13 +164,24 @@ function encode_and_pad(charmap::CharMap, songtext::String,
                         cfg::ModelConfig)::Array{Int, 1}
     """
     Maps the characters in a song to their integer 
-    representations, and pads its length to the in length in the config.
+    representations, and pads its length to the in length in the config 
+    (if it's shorter than cfg.in) or truncates it down to that length 
+    (if it's longer).
     """
-
     unpadded::Array{Int, 1} = encode(charmap, songtext)
+    
     remaining::Int = cfg.in - length(unpadded)
-    padding = repeat([cfg.pad], remaining)
-    return vcat(unpadded, padding)   
+    if length(unpadded) <= cfg.in
+        padding = repeat([cfg.pad], remaining)
+        result = vcat(unpadded, padding)
+    else
+        ## Longer than the configured input dim; truncate to make it match
+        result = unpadded[1:cfg.in]
+    end
+    if length(result) != cfg.in
+        error("bad input length calculation")
+    end
+    return result
 end
 
 function encode_and_pad(charmap::CharMap, songtexts::Array{String, 1},
@@ -203,7 +222,8 @@ function construct_model(in::Int, out::Int)
                  Dense(out, out, tanh),
                  Dense(out, out, relu),
                  Dense(out, out, tanh),
-                 Dense(out, out, softmax))
+                 Dense(out, out),
+                 softmax)
 end
 
 construct_model(cfg::ModelConfig) = construct_model(cfg.in, cfg.out)
@@ -211,19 +231,21 @@ construct_model(cfg::ModelConfig) = construct_model(cfg.in, cfg.out)
 data(mdata::ModelData) = zip(mdata.X, mdata.Y)
 
 function Flux.train!(m::Flux.Chain, trn::ModelData, tst::ModelData)
-    loss(x, y) = crossentropy(m(x), y)
+    loss(x, y) = Flux.crossentropy(m(x), y)
     opt = Flux.ADAM()
     trn_batches = Flux.Data.DataLoader(trn.X, trn.Y, batchsize = 1024) 
     evalcb() = @show(loss(tst.X, tst.Y))
     
-    Flux.train!(loss, params(m), trn_batches, opt, cb = Flux.throttle(evalcb, 5))
+    Flux.train!(loss, params(m), trn_batches, opt,
+                cb = Flux.throttle(evalcb, 5))
 end
 
 function getvars()
     df = read_and_process_data()
     ## TODO: need to match input len between train and test.
     trn = ModelData(filter(row -> row[:group] == TRN_IND, df))
-    tst = ModelData(filter(row -> row[:group] == TST_IND, df))
+    tst = ModelData(filter(row -> row[:group] == TST_IND, df),
+                    trn.cfg)
     m = construct_model(trn.cfg)
     return df, trn, tst, m
 end
